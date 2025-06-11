500446d73db958d32e274e9035662ba09c7ce1ce
Sharukh-Ahmed <sharukhahmed0706@gmail.com>
2025-06-11 18:51:02 +0530

    br4

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -45,7 +45,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://bookstore-backend-itmj.onrender.com/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book edited Successfully', { variant: 'success' });
